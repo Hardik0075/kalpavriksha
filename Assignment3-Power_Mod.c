bbc49913c7f363ee2f447adf8d0ccba4370cad45
Hardik0075 <110181411+Hardik0075@users.noreply.github.com>
2025-01-01 10:54:51 +0530

    Assignment3-Power_Mod.c

---
+++ Assignment3-Power_Mod.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+long long calculate(long long base, long long exp , long long mod){
+    long long result=1;
+    base = base % mod;
+    while(exp>0){
+        if(exp%2==1){
+            result = (result*base)%mod;
+        }
+        exp = exp/2;
+        base = (base * base)% mod;
+    }
+    return result;
+}
+int main(){
+    long long base,exp,mod,result;
+    scanf("%lld",&base);
+    scanf("%lld",&exp);
+    scanf("%lld",&mod);
+    if(mod==0){
+        printf("0");
+        return 0;
+    }
+    if(exp==0){
+        printf("1");
+        return 0;
+    }   
+    result =  calculate(base,exp,mod);
+    printf("%lld",result);
+}
