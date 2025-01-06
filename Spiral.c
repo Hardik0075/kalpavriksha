7bf4196834c2b1295fcf0c1e542e028bc4348552
Hardik0075 <110181411+Hardik0075@users.noreply.github.com>
2025-01-06 14:38:20 +0530

    Spiral.c

--- Spiral.c
+++ Spiral.c
@@ -1,49 +1,61 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    int rows , columns;
-    scanf("%d",&rows);
-    scanf("%d",&columns);
+int main()
+{
+    int rows, columns;
+    scanf("%d", &rows);
+    scanf("%d", &columns);
     int *array[rows];
-    for(int index=0;index<rows;index++){
-        array [index] = (int *)malloc(columns*sizeof(int));
+    for (int index = 0; index < rows; index++)
+    {
+        array[index] = (int *)malloc(columns * sizeof(int));
     }
-    for(int index1=0;index1<rows;index1++){
-        for(int index2=0;index2<columns;index2++){
-            scanf("%d",(*(array+index1)+index2));
+    for (int index1 = 0; index1 < rows; index1++)
+    {
+        for (int index2 = 0; index2 < columns; index2++)
+        {
+            scanf("%d", (*(array + index1) + index2));
         }
     }
-    for(int index1=0;index1<rows;index1++){
-        for(int index2=0;index2<columns;index2++){
-            printf("%d ",*(*(array+index1)+index2));
+    for (int index1 = 0; index1 < rows; index1++)
+    {
+        for (int index2 = 0; index2 < columns; index2++)
+        {
+            printf("%d ", *(*(array + index1) + index2));
         }
         printf("\n");
     }
 
     int row_start = 0;
-    int row_end = rows-1;
+    int row_end = rows - 1;
     int columns_start = 0;
-    int columns_end = columns-1;
-    while(row_start<=row_end && columns_start<=columns_end){
-        for(int index=columns_start;index<=columns_end;index++){
-            printf("%d ",*(*(array+row_start)+index));                     
+    int columns_end = columns - 1;
+    while (row_start <= row_end && columns_start <= columns_end)
+    {
+        for (int index = columns_start; index <= columns_end; index++)
+        {
+            printf("%d ", *(*(array + row_start) + index)); // array[row_start][index]
         }
         row_start++;
-        for(int index=row_start;index<=row_end;index++){
-            printf("%d ",*(*(array+index)+columns_end));
+        for (int index = row_start; index <= row_end; index++)
+        {
+            printf("%d ", *(*(array + index) + columns_end));
         }
         columns_end--;
-        for(int index=columns_end;index>=columns_start;index--){
-            printf("%d ",*(*(array+row_end)+index));
+        for (int index = columns_end; index >= columns_start; index--)
+        {
+            printf("%d ", *(*(array + row_end) + index));
         }
         row_end--;
-        for(int index=row_end;index>=row_start;index--){
-            printf("%d ",*(*(array+index)+columns_start));
+        for (int index = row_end; index >= row_start; index--)
+        {
+            printf("%d ", *(*(array + index) + columns_start));
         }
         columns_start++;
-    }    
-    for(int i=0;i<rows;i++){
+    }
+    for (int i = 0; i < rows; i++)
+    {
         free(array[i]);
     }
     return 0;
