d49ba2f787d86b34d2caaac964d8543f637a66f4
Hardik0075 <110181411+Hardik0075@users.noreply.github.com>
2025-01-09 23:13:52 +0530

    code fomatting improved

--- Check_for_substring_in_array_of_string.c
+++ Check_for_substring_in_array_of_string.c
@@ -8,19 +8,16 @@ int length(char array[]) {
 	}
 	return count;
 }
-
 int main()
 {
 	int inputs;
 	scanf("%d",&inputs);
 	printf("Enter String: \n");
 	char *array[inputs];
-	for(int i=0; i<inputs; i++) 
-  {
+	for(int i=0; i<inputs; i++) {
 		array[i]=(char *)malloc(sizeof(char)*50);
 	}
-	for(int i=0; i<inputs; i++) 
-  {
+	for(int i=0; i<inputs; i++) {
 		scanf("%s",*(array+i));
 	}
 	char substring[50];
@@ -28,25 +25,19 @@ int main()
 	scanf("%s",substring);
 	int result_count=0;
 	printf("result : \n");
-	for(int outer_loop=0; outer_loop<inputs; outer_loop++) 
-  {
-		for(int inner_loop=0; inner_loop<length(array[outer_loop]); inner_loop++) 
-    {
-			if(array[outer_loop][inner_loop]==substring[0]) 
-      {
+	for(int outer_loop=0; outer_loop<inputs; outer_loop++) {
+		for(int inner_loop=0; inner_loop<length(array[outer_loop]); inner_loop++) {
+			if(array[outer_loop][inner_loop]==substring[0]) {
 				int substring_incrementor=0,main_string_incrementor=0;
 				int match_count=0;
-				while(substring_incrementor<length(substring) && inner_loop<length(array[outer_loop]) && match_count<=length(substring)) 
-        {
-					if(substring[substring_incrementor]==array[outer_loop][inner_loop+main_string_incrementor]) 
-          {
+				while(substring_incrementor<length(substring) && inner_loop<length(array[outer_loop]) && match_count<=length(substring)) {
+					if(substring[substring_incrementor]==array[outer_loop][inner_loop+main_string_incrementor]) {
 						match_count++;
 					}
 					substring_incrementor++;
 					main_string_incrementor++;
 				}
-				if(match_count==length(substring)) 
-        {
+				if(match_count==length(substring)) {
 					result_count++;
 					printf("%s\n",*(array+outer_loop));
 					break;
