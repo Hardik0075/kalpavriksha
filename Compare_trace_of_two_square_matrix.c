81c4f3e0ea7120760ea9c2937874e0b290f51590
Hardik0075 <110181411+Hardik0075@users.noreply.github.com>
2025-01-10 09:15:47 +0530

    added memory free functionality

--- Compare_trace_of_two_square_matrix.c
+++ Compare_trace_of_two_square_matrix.c
@@ -2,35 +2,39 @@
 #include <stdlib.h>
 int main()
 {
-    //trace
-    int dimension;
-    scanf("%d",&dimension);
-    int *array1[dimension];
-    int *array2[dimension];
-    for(int index=0;index<dimension;index++){
-        array1[index]= (int *)malloc(sizeof(int)*dimension);
-        array2[index]= (int *)malloc(sizeof(int)*dimension);
-    }
-    for(int outer_index=0;outer_index<dimension;outer_index++){
-        for(int inner_index=0;inner_index<dimension;inner_index++){
-            scanf("%d",(*(array1+outer_index)+inner_index));
-        }
-    }
-    for(int outer_index=0;outer_index<dimension;outer_index++){
-        for(int inner_index=0;inner_index<dimension;inner_index++){
-            scanf("%d",(*(array2+outer_index)+inner_index));
-        }
-    }
-    int trace1=0,trace2=0;
-    for(int index=0;index<dimension;index++){
-                trace1+= *(*(array1+index)+index);
-                trace2+= *(*(array2+index)+index);
-    }
-    if(trace1==trace2){
-        printf("Both Trace are equal \n");
-    }
-    else{
-        printf("Trace is not equal \n");
-    }
-    return 0;
+	//trace
+	int dimension;
+	scanf("%d",&dimension);
+	int *array1[dimension];
+	int *array2[dimension];
+	for(int index=0; index<dimension; index++) {
+		array1[index]= (int *)malloc(sizeof(int)*dimension);
+		array2[index]= (int *)malloc(sizeof(int)*dimension);
+	}
+	for(int outer_index=0; outer_index<dimension; outer_index++) {
+		for(int inner_index=0; inner_index<dimension; inner_index++) {
+			scanf("%d",(*(array1+outer_index)+inner_index));
+		}
+	}
+	for(int outer_index=0; outer_index<dimension; outer_index++) {
+		for(int inner_index=0; inner_index<dimension; inner_index++) {
+			scanf("%d",(*(array2+outer_index)+inner_index));
+		}
+	}
+	int trace1=0,trace2=0;
+	for(int index=0; index<dimension; index++) {
+		trace1+= *(*(array1+index)+index);
+		trace2+= *(*(array2+index)+index);
+	}
+	if(trace1==trace2) {
+		printf("Both Trace are equal \n");
+	}
+	else {
+		printf("Trace is not equal \n");
+	}
+	for(int index=0; index<dimension; index++) {
+		free(array1[index]);
+		free(array2[index]);
+	}
+	return 0;
 }
