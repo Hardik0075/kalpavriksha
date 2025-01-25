616dd5872c10c322796327313d555d4f9e0e39ee
Hardik Sharma <hardik.s@intimetec.com>
2025-01-25 17:22:46 +0530

    Refactor memory management and improved function modularity ,also updated code to to check input validation and naming and casing

--- occurence_of_character_in_string.c
+++ occurence_of_character_in_string.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int length(char array[])
+int length_of_array(char array[])
 {
     int count = 0;
     while (array[count] != '\0')
@@ -11,52 +11,86 @@ int length(char array[])
     return count;
 }
 
-int main()
+void free_memory_allocated_to_array(char **array, int size)
 {
-    int input;
-    scanf("%d", &input);
-    char *array[input];
-    for (int iterator = 0; iterator < input; iterator++)
-    {
-        array[iterator] = (char *)malloc(sizeof(char) * 50);
-    }
-    printf("Enter strings : \n");
-    for (int index = 0; index < input; index++)
-    {
-        scanf("%s", array[index]);
-    }
-
-    printf("Enter Character: \n");
-    char character;
-    scanf(" %c", &character);
-    int max_count = 0, flag = 0;
-    char *max_string;
-    max_string = (char *)malloc(sizeof(char) * 50);
-    for (int index = 0; index < input; index++)
-    {
-        int count = 0;
-        for (int inner_index = 0; inner_index < length(array[index]); inner_index++)
-        {
-            if (*(*(array + index) + inner_index) == character)
-            {
-                count++;
-                flag = 1;
-            }
-        }
-        if (count > max_count)
-        {
-            max_count = count;
-            max_string = array[index];
-        }
-    }
-    if (flag == 1)
-        printf("%s\n", max_string);
-    else
-        printf("No Match Found \n");
-
-    for (int iterator = 0; iterator < input; iterator++)
+    for (int iterator = 0; iterator < size; iterator++)
     {
         free(array[iterator]);
+        array[iterator] = NULL;
     }
+    free(array);
+    array = NULL;
+}
+
+int find_maximum_occurrences(char **array, int input, char character_to_match)
+{
+    int maximum_count = 0;
+    int maximum_occurence_string_index = -1;
+    int count = 0;
+    for (int index = 0; index < input; index++)
+    {
+        count = 0;
+        for (int string_iterator = 0; string_iterator < length_of_array(array[index]); string_iterator++)
+        {
+            if (array[index][string_iterator] == character_to_match)
+            {
+                count++;
+            }
+        }
+        if (count > maximum_count)
+        {
+            maximum_count = count;
+            maximum_occurence_string_index = index;
+        }
+    }
+    return maximum_occurence_string_index;
+}
+
+int main()
+{
+    int number_of_input;
+    printf("Enter number of strings: ");
+    if (scanf("%d", &number_of_input) != 1 || number_of_input <= 0)
+    {
+        printf("Invalid input. Exiting.\n");
+        return 1;
+    }
+
+    char **array = (char **)malloc(sizeof(char *) * number_of_input);
+    if (array == NULL)
+    {
+        printf("Memory allocation failed. Exiting.\n");
+        return 1;
+    }
+
+    printf("Enter strings:\n");
+    for (int iterator = 0; iterator < number_of_input; iterator++)
+    {
+        array[iterator] = (char *)malloc(sizeof(char) * 50);
+        if (array[iterator] == NULL)
+        {
+            printf("Memory allocation for string failed. Exiting.\n");
+            free_memory_allocated_to_array(array, iterator);
+            return 1;
+        }
+        scanf("%s", array[iterator]);
+    }
+
+    printf("Enter character to find occurrences: ");
+    char character_to_match;
+    scanf(" %c", &character_to_match);
+
+    int max_index = find_maximum_occurrences(array, number_of_input, character_to_match);
+    if (max_index != -1)
+    {
+        printf("String with most occurrences of '%c': %s\n", character_to_match, array[max_index]);
+    }
+    else
+    {
+        printf("No Match Found\n");
+    }
+
+    free_memory_allocated_to_array(array, number_of_input);
+
     return 0;
 }
