888930ba6c50e8060cd2773f7cf08d7e3bf0a245
Hardik0075 <110181411+Hardik0075@users.noreply.github.com>
2025-01-31 14:49:29 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-This is for Kalpavriksha
+Here we are implementing queue using array .
+Enqueue():
+a. If the queue is full (rear == size_of_queue - 1), we print Queue Overflow.
+b. If inserting the first element, front is set to 0 .
+c. The new element is inserted at rear + 1, and rear is incremented.
+Dequeue():
+a. If front > rear, the queue is empty (Underflow).
+b. The front element is printed and removed by incrementing front.
+c. If the last element is dequeued, both front and rear are reset to -1.
+Peek_of_queue() :
+a. If the queue is empty, print a message.
+b. Otherwise, print the element at front (the first element in the queue).
+is_queue_empty():
+a. If front > rear, the queue is empty.
+b. Otherwise, the queue is not empty.
+size_of_queue():
+a. The queue size is rear - front + 1.
+b. If the queue is empty, print a message.
+main():
+a. The user enters the queue size.
+b. A loop runs until the user selects Exit (6).
+c. The user chooses an operation (1 to 6), and the respective function is called.
+
+Time and Space Complexity :
+Time Complexity of all the function is O(1) and whole code O(m) , m is number of operation.
+Space complexity of function is O(1) and whole code is O(n), where n is size of queue.
---
+++ queue_implementation_using_array.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+
+void enqueue(int queue[], int *front, int *rear, int value_to_insert_in_queue, int size_of_queue)
+{
+    if (*rear == size_of_queue - 1)
+    {
+        printf("Queue Overflow\n");
+        return;
+    }
+    if (*front == -1)
+        *front = 0;
+
+    queue[++(*rear)] = value_to_insert_in_queue;
+    printf("%d inserted into queue\n", value_to_insert_in_queue);
+}
+
+void dequeue(int queue[], int *front, int *rear)
+{
+    if (*front == -1 || *front > *rear)
+    {
+        printf("Queue Underflow\n");
+        return;
+    }
+    printf("Removed element: %d\n", queue[*front]);
+
+    if (*front == *rear)
+    {
+        *front = -1;
+        *rear = -1;
+    }
+    else
+    {
+        (*front)++;
+    }
+}
+
+void peek_of_queue(int queue[], int front, int rear)
+{
+    if (front == -1 || front > rear)
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("Peek element is %d\n", queue[front]);
+}
+
+void is_queue_empty(int front, int rear)
+{
+    if (front == -1 || front > rear)
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("Queue is not empty\n");
+}
+
+void queue_size(int front, int rear)
+{
+    if (front == -1 || front > rear)
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("Size of queue is %d\n", rear - front + 1);
+}
+
+int main()
+{
+    int size_of_queue;
+    printf("Enter size of queue: ");
+    scanf("%d", &size_of_queue);
+
+    int queue[size_of_queue];
+    int front = -1, rear = -1;
+
+    while (1)
+    {
+        printf("\nEnter the choice you want to implement on queue:\n");
+        printf("1. Enqueue\n2. Dequeue\n3. Peek\n4. Is queue empty\n5. Size of queue\n6. Exit\n");
+        printf("Enter choice: ");
+        int operation_user_want_to_apply;
+        scanf("%d", &operation_user_want_to_apply);
+
+        if (operation_user_want_to_apply == 1)
+        {
+            int value_to_insert_in_queue;
+            printf("Enter value to insert in queue: ");
+            scanf("%d", &value_to_insert_in_queue);
+            enqueue(queue, &front, &rear, value_to_insert_in_queue, size_of_queue);
+        }
+        else if (operation_user_want_to_apply == 2)
+        {
+            dequeue(queue, &front, &rear);
+        }
+        else if (operation_user_want_to_apply == 3)
+        {
+            peek_of_queue(queue, front, rear);
+        }
+        else if (operation_user_want_to_apply == 4)
+        {
+            is_queue_empty(front, rear);
+        }
+        else if (operation_user_want_to_apply == 5)
+        {
+            queue_size(front, rear);
+        }
+        else
+        {
+            break;
+        }
+    }
+    return 0;
+}
