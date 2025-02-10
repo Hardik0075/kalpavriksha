dc70dfa0b854b62ded8bdbebe42d29926fd36089
Hardik0075 <110181411+Hardik0075@users.noreply.github.com>
2025-02-10 11:40:30 +0530

    Fix: Improved queue overflow condition , improved output messages

--- queue_implementation_using_two_stack.c
+++ queue_implementation_using_two_stack.c
@@ -2,26 +2,31 @@
 
 void enqueue(int stack1[], int *top_of_stack1, int maximum_size_of_stack, int value_to_insert)
 {
-    if (*top_of_stack1 == maximum_size_of_stack)
+    if (*top_of_stack1 == maximum_size_of_stack - 1)
     {
-        printf("overflow will \n");
+        printf("Queue Overflow: Cannot insert %d, the queue is full.\n", value_to_insert);
         return;
     }
     stack1[++(*top_of_stack1)] = value_to_insert;
+    printf("Enqueued: %d\n", value_to_insert);
 }
 
 void dequeue(int stack1[], int *top_of_stack1, int stack2[], int *top_of_stack2)
 {
     if (*top_of_stack1 == -1)
     {
-        printf("underflow will \n");
+        printf("Queue Underflow: No elements to dequeue.\n");
         return;
     }
+    
     while (*top_of_stack1 != -1)
     {
         stack2[++(*top_of_stack2)] = stack1[(*top_of_stack1)--];
     }
-    --(*top_of_stack2);
+    
+    int dequeued_value = stack2[(*top_of_stack2)--];
+    printf("Dequeued: %d\n", dequeued_value);
+
     while (*top_of_stack2 != -1)
     {
         stack1[++(*top_of_stack1)] = stack2[(*top_of_stack2)--];
@@ -32,59 +37,73 @@ void peek(int stack1[], int top_of_stack1, int stack2[], int top_of_stack2)
 {
     if (top_of_stack1 == -1)
     {
-        printf("Nothing\n");
+        printf("Queue is Empty: No elements to peek.\n");
         return;
     }
+
     while (top_of_stack1 != -1)
     {
         stack2[++(top_of_stack2)] = stack1[(top_of_stack1)--];
     }
-    printf("peak is %d\n", stack2[top_of_stack2]);
+    
+    printf("Front Element (Peek): %d\n", stack2[top_of_stack2]);
+
     while (top_of_stack2 != -1)
     {
         stack1[++(top_of_stack1)] = stack2[(top_of_stack2)--];
     }
 }
 
-void is_empty(int stack[], int top)
+void is_empty(int top)
 {
     if (top == -1)
     {
-        printf("Yes it empty\n");
-        return;
-    }
-    printf("No its not empty\n");
-}
-
-void find_size(int stack[], int top)
-{
-    if (top == -1)
-    {
-        printf("size of queue is 0\n");
+        printf("Queue Status: Empty\n");
     }
     else
     {
-        printf("size of queue is %d\n", top + 1);
+        printf("Queue Status: Not Empty\n");
+    }
+}
+
+void find_size(int top, int maximum_size_of_stack)
+{
+    if (top == -1)
+    {
+        printf("Queue is empty\n");
+    }
+    else
+    {
+        printf("Queue Size: %d\n", top + 1);
     }
 }
 
 int main()
 {
     int maximum_size_of_stack;
-    printf("Enter Maximum size of stacks : ");
+    printf("Enter Maximum Queue Size: ");
     scanf("%d", &maximum_size_of_stack);
+    
     int stack1[maximum_size_of_stack], stack2[maximum_size_of_stack];
     int top_of_stack1 = -1, top_of_stack2 = -1;
+
     while (1)
     {
         int choice;
-        printf("Enter choice (1-Enqueue, 2-Dequeue, 3-Peek of queue , 4-Is queue empty, 5-Find Size of queue, 6-Exit): ");
-        printf("Enter Choice : ");
+        printf("Queue Operations:\n");
+        printf("1 - Enqueue\n");
+        printf("2 - Dequeue\n");
+        printf("3 - Peek\n");
+        printf("4 - Check if Empty\n");
+        printf("5 - Find Size\n");
+        printf("6 - Exit\n");
+        printf("Enter Choice: ");
         scanf("%d", &choice);
+
         if (choice == 1)
         {
             int value_to_insert;
-            printf("enter number to insert : ");
+            printf("Enter value to insert: ");
             scanf("%d", &value_to_insert);
             enqueue(stack1, &top_of_stack1, maximum_size_of_stack, value_to_insert);
         }
@@ -98,16 +117,22 @@ int main()
         }
         else if (choice == 4)
         {
-            is_empty(stack1, top_of_stack1);
+            is_empty(top_of_stack1);
         }
         else if (choice == 5)
         {
-            find_size(stack1, top_of_stack1);
+            find_size(top_of_stack1, maximum_size_of_stack);
+        }
+        else if (choice == 6)
+        {
+            printf("Exiting Program...\n");
+            break;
         }
         else
         {
-            break;
+            printf("Invalid Choice! Please enter a valid option.\n");
         }
     }
+
     return 0;
 }
