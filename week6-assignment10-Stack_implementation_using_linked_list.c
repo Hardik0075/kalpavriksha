188cc5f05ac419ba28b85b76a6c971fecab137b4
Hardik0075 <110181411+Hardik0075@users.noreply.github.com>
2025-02-10 10:44:49 +0530

    Fix : Added Free list function , added free function while popping

--- week6-assignment10-Stack_implementation_using_linked_list.c
+++ week6-assignment10-Stack_implementation_using_linked_list.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 typedef struct node
 {
     int data;
@@ -15,16 +16,8 @@ void push_in_stack(node **head, int *top, int value_to_push_in_stack)
         return;
     }
     new_node->data = value_to_push_in_stack;
-    new_node->next = NULL;
-    if (*head == NULL)
-    {
-        *head = new_node;
-    }
-    else
-    {
-        new_node->next = *head;
-        *head = new_node;
-    }
+    new_node->next = *head;
+    *head = new_node;
     (*top)++;
 }
 
@@ -32,20 +25,13 @@ void pop_from_stack(node **head, int *top)
 {
     if (*head == NULL)
     {
-        printf("Will underflow \n");
+        printf("Stack Underflow\n");
         return;
     }
-    else if ((*head)->next == NULL)
-    {
-        *head = NULL;
-    }
-    else
-    {
-        node *temp = *head;
-        *head = (*head)->next;
-        free(temp);
-    }
 
+    node *temp = *head;
+    *head = (*head)->next;
+    free(temp);
     (*top)--;
 }
 
@@ -53,45 +39,59 @@ void peek_of_stack(node *head)
 {
     if (head == NULL)
     {
-        printf("Empty stack \n");
+        printf("Stack is Empty\n");
     }
     else
     {
-        printf("peak  is %d\n", head->data);
+        printf("Top element is %d\n", head->data);
     }
 }
 
-void is_stack_empty(int top)
+void is_stack_empty(node *head)
 {
-    if (top == -1)
+    if (head == NULL)
     {
         printf("Stack is Empty\n");
     }
     else
     {
-        printf("Not empty\n");
+        printf("Stack is Not Empty\n");
     }
 }
 
 void size_of_stack(int top)
 {
-    printf("%d is the size of stack \n", top + 1);
+    printf("Stack size: %d\n", top + 1);
+}
+
+void free_list(node *head)
+{
+    node *temp;
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+    head = NULL;
 }
 
 int main()
 {
     node *head = NULL;
     int top = -1;
+
     while (1)
     {
-        printf("Your choices are :\n1.Push in stack\n2.Pop from stack\n3.Peek of stack\n4.is stack empty\n5.Size of stack\n6.Exit\n");
+        printf("\nYour choices are:\n1. Push in stack\n2. Pop from stack\n3. Peek of stack\n4. Check if stack is empty\n5. Size of stack\n6. Exit\n");
         int choice;
         printf("Enter Choice: ");
         scanf("%d", &choice);
+
         if (choice == 1)
         {
             int value_to_push_in_stack;
-            printf("Enter value to push in to stack : ");
+            printf("Enter value to push into stack: ");
             scanf("%d", &value_to_push_in_stack);
             push_in_stack(&head, &top, value_to_push_in_stack);
         }
@@ -105,17 +105,22 @@ int main()
         }
         else if (choice == 4)
         {
-            is_stack_empty(top);
+            is_stack_empty(head);
         }
         else if (choice == 5)
         {
             size_of_stack(top);
         }
-        else
+        else if (choice == 6)
         {
             break;
         }
+        else
+        {
+            printf("Invalid Choice! Try Again.\n");
+        }
     }
 
+    free_list(head);
     return 0;
 }
